e8ac86447e92c9b62a9925da146f8cd370a7534a
Damian Tatarowicz <damian.tatarowicz@gmail.com>
2019-05-12 10:26:02 +0100

    add recursive solution

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,18 +2,28 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        int tab[] = new int [20];
-        tab [0] = 0;
-        tab [1] = 1;
-        for (int i=2; i<tab.length; i++){
-            tab[i] = tab[i-1]+tab[i-2];
+public class App {
+    public static void main(String[] args) {
+        int tab[] = new int[20];
+        tab[0] = 0;
+        tab[1] = 1;
+        for (int i = 2; i < tab.length; i++) {
+            tab[i] = tab[i - 1] + tab[i - 2];
             System.out.println(tab[i]);
+
+
+            System.out.println(fibbo(5));
         }
     }
+
+    public static int fibbo(int a) {
+        if (a <= 2) {
+            return 1;
+        } else {
+            return fibbo(a - 2) + fibbo(a - 1);
+        }
+
+    }
 }
+
